5a69122cf436acbaa63e8fa72fada07ee6bb4cc1
vishalgautam01212 <ivishalgautam01@gmail.com>
2022-03-07 18:26:47 +0530

    adding css

--- src/components/Cards/Cards/Cards.jsx
+++ src/components/Cards/Cards/Cards.jsx
@@ -14,7 +14,7 @@ const Cards =({data : { confirmed, recovered, deaths, lastUpdate} } )=> {
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} className={cx(styles.test, styles.test1)} >
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)} >
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -24,7 +24,7 @@ const Cards =({data : { confirmed, recovered, deaths, lastUpdate} } )=> {
                         <Typography variant="body2">Number of Active Cases of COVID-19</Typography>
                     </CardContent>
                  </Grid>
-                 <Grid item component={Card}>
+                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)} >
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -34,7 +34,7 @@ const Cards =({data : { confirmed, recovered, deaths, lastUpdate} } )=> {
                         <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                  </Grid>
-                 <Grid item component={Card}>
+                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)} >
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Deaths!</Typography>
                         <Typography variant="h5">
--- src/components/Cards/Charts/Charts.jsx
+++ src/components/Cards/Charts/Charts.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
-const Charts=()=>{
-    return(
-        <h1>Charts</h1>
-    )
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 
+import styles from  './Chart.module.css';
+
+const Charts=()=>{
+    const {dailyData, setDailyData} = useState({});
+
+    useEffect(() => {
+        const fetchAPI  = async () =>{
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const  lineChart = (
+        <Line
+            data={{
+                labels: '';
+                datasets: [{}, {}],
+            }}
+        />
+    );
+
+    return(
+        <h1>Chart</h1>
+    )
 }
-export default Charts;
+export default Chart;
