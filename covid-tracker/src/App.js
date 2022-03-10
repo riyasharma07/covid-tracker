bb26967bacbe2b2b3c560284b1b9c1977f1f7aaf
vishalgautam01212 <ivishalgautam01@gmail.com>
2022-03-10 22:44:49 +0530

    del

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Charts } from './components/Cards';
+import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
-import image from './image.png';
+import image from './images/image.png';
 
 class App extends React.Component {
   state = {
@@ -32,7 +32,7 @@ class App extends React.Component {
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Charts data={data} country={country} /> 
+        <Chart data={data} country={country} /> 
       </div>
     );
   }
